1300a72e350d9d6d2a45caa44aa798e310c356aa
Wilson Reid <wilsonthehungryman@gmail.com>
2025-11-10 08:38:58 -0600

    Created some recovery communities in the seeds file

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -1,5 +1,5 @@
 class Meeting < ApplicationRecord
   belongs_to :recovery_community
-  belongs_to :location
-  belongs_to :join_info
+  belongs_to :location, optional: true
+  belongs_to :join_info, optional: true
 end
