eeb2a66364051b363e9bd87fb8b492b8a6f3567a
Wilson Reid <wilsonthehungryman@gmail.com>
2025-11-10 09:57:56 -0600

    Fixed the meeting time for the dharma meetings

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,9 +24,45 @@ recovery_dharma_wednesday_location = Location.find_or_create_by(
 
 Meeting.find_or_create_by(
   source: "Manual",
-  time: Time.new(2025, 11, 12, 7, 0, 0, winnipeg_timezone),
+  time: Time.new(2025, 11, 12, 19, 0, 0, winnipeg_timezone),
   meeting_type: "In Person",
   recurring: true,
   recovery_community: recovery_dharma,
   location: recovery_dharma_wednesday_location
+)
+
+recovery_dharma_thursday_location = Location.find_or_create_by(
+  name: "Centre for Spiritual Living",
+  street_address: "625 Spruce Street",
+  city: "Winnipeg",
+  province: "Manitoba",
+  country: "Canada",
+  wheelchair_accessible: false
+)
+
+Meeting.find_or_create_by(
+  source: "Manual",
+  time: Time.new(2025, 11, 13, 19, 30, 0, winnipeg_timezone),
+  meeting_type: "In Person",
+  recurring: true,
+  recovery_community: recovery_dharma,
+  location: recovery_dharma_thursday_location
+)
+
+recovery_dharma_saturday_location = Location.find_or_create_by(
+  name: "Earl Grey Community Centre",
+  street_address: "360 Cockburn Street North",
+  city: "Winnipeg",
+  province: "Manitoba",
+  country: "Canada",
+  wheelchair_accessible: false
+)
+
+Meeting.find_or_create_by(
+  source: "Manual",
+  time: Time.new(2025, 11, 15, 9, 30, 0, winnipeg_timezone),
+  meeting_type: "In Person",
+  recurring: true,
+  recovery_community: recovery_dharma,
+  location: recovery_dharma_saturday_location
 )
